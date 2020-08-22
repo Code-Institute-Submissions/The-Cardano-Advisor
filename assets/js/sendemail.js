411bd4ac9bdda9422e55a2040b72fb8c0855648f
MarcSparks7 <Marc.Brendan.veldhuis@gmail.com>
2020-08-22 12:50:18 +0000

    added emailjs to contact from, added sendemail.js file with code to send eamil

---
+++ assets/js/sendemail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "cardano", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "advice_request": contactForm.requests.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
