f66c0290194c747e478354ecb7230db8696a967d
MarcSparks7 <Marc.Brendan.veldhuis@gmail.com>
2020-08-26 18:12:23 +0000

    trying to get modal and roadmap js to work, added in different possible outcomes, still testing

--- assets/js/roadmap.js
+++ assets/js/roadmap.js
@@ -1,1 +1,10 @@
-document.getElementById('newmain').src='images/byron.jpg'
+const main = document.querySelector('#newmain')
+const byron = document.querySelector('#byron');
+const shelley = document.querySelector('#shelley');
+const basho = document.querySelector('#basho');
+const gougen = document.querySelector('#gougen');
+const voltaire = document.querySelector('#voltaire');
+
+byron.addEventListener('click', () => {
+    src = '../images/shelley-l.jpg'
+})
