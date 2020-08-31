076aa7947bcd013eacf9a8694c1442d7ea27a15e
MarcSparks7 <Marc.Brendan.veldhuis@gmail.com>
2020-08-31 21:03:26 +0000

    trying to get data from JSON file into graph

--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,12 +1,20 @@
 
-function chatIt () {
+let api = 'https://api.coingecko.com/api/v3/coins/cardano/market_chart?vs_currency=eur&days=7'
+async function getPrice () {
+    const response = await fetch(api);
+    const data =await response.json();
+    console.log(data.prices)
+};
+
+getPrice()
+
     const ctx = document.getElementById('myChart');
     const myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: 'Cardano Weekly Price',
+                label: 'Cardano 7 day Price',
                 data: [12, 19, 3, 5, 2, 3],
                 
             }]
@@ -21,14 +29,5 @@ function chatIt () {
             }
         }
     });
-}
 
-let api = 'https://api.coingecko.com/api/v3/coins/cardano/market_chart?vs_currency=eur&days=7'
-async function getPrice () {
-    const response = await fetch(api);
-    const data =await response.json();
 
-    console.log(data.prices);
-};
-
-getPrice()
